799de5aba7cb5ed76e5be0b181a6e6292b1940d9
robert neuzil <robertneuzil@gmail.com>
2018-12-07 22:02:17 -0500

    five minutes

---
+++ 389.py
@@ -0,0 +1,37 @@
+'''
+
+Input:
+s = "abcd"
+t = "abcde"
+
+Output:
+e
+
+Explanation:
+'e' is the letter that was added.
+
+
+Input:
+s = "abcd"
+t = "abcde"
+
+Output:
+e
+
+Explanation:
+'e' is the letter that was added.
+
+'''
+
+s = "abcd"
+t = "abcdefghi"
+
+s_list = [x for x in s]
+t_list = [y for y in t]
+
+difference = (set(s_list) & set(t_list))
+
+
+for c in t_list:
+	if c not in difference:
+		print (c)
