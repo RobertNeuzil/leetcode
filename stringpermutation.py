af816e23318843776f40e6f39b2eae3d79ce2336
robert neuzil <robertneuzil@gmail.com>
2018-12-23 00:58:55 -0500

    two sum

--- stringpermutation.py
+++ stringpermutation.py
@@ -1,13 +1,10 @@
 '''
 A permutation == is a reording of a string that includes all the letters of the original string,
 even if they are not in the same order.
-
 input: "television"
 output : "eltevision", "noisivelet"     are both valid.
 output : "televis" is not valid
-
 write a function to decide whether one string is a valid permutation of the other
-
 '''
 
 string_one = 'television'
@@ -17,22 +14,22 @@ string_four = 'the         moon jumps o v e r theco     w'
 string_five = "It was a long and windy road"
 string_six = "I went to mcdonald's and got a hamburger and a kid's toy"
 
-def is_it_valid(one, two):
-	one = one.replace(' ', '')  # get rid of spaces
-	two = two.replace(' ', '')  # get rid of spaces
-	# or don't get rid of spaces and treat extra spaces as not valid:
-	# in that case, skip the code and they will be evaluated to false at line 25
-	if len(one) != len(two):
-		return False
 
-	
-	for c in one:
-		if c in two:
-			two = two.replace(c, "")
-	if len(two) == 0:
-		return True
-	else:
-		return False
+def is_it_valid(one, two):
+    one = one.replace(' ', '')  # get rid of spaces
+    two = two.replace(' ', '')  # get rid of spaces
+    # or don't get rid of spaces and treat extra spaces as not valid:
+    # in that case, skip line 18 & 19 and they will be evaluated to false at line 25
+    if len(one) != len(two):
+        return False
+    for c in one:
+        if c in two:
+            two = two.replace(c, "")
+    if len(two) == 0:
+        return True
+    else:
+        return False
+
 
 print(is_it_valid(string_one, string_two))
 print(is_it_valid(string_five, string_six))
--- twosumtargetvalue.py
+++ twosumtargetvalue.py
@@ -1,24 +1,17 @@
+
 # YouTube Video: https://www.youtube.com/watch?v=gCin6Qz-eJQ
 # Given an array of integers, return the two numbers such that they add up to
 # a specific target. You may assume that each input would have exactly one
 # solution, and you may not use the same element twice.
 
 A = [-2, 1, 2, 4, 7, 11]
-target = 20
+target = 13
 
-"""
 
-def two_sum_brute_force(A, target):
-    for i in range(len(A)-1):  # range(0, 5) 0,1,2,3,4
-        for j in range(i+1, len(A)): # range(1, 6) range(2, 6) range(3, 6) range(4, 6) range(5, 6)
-            if A[i] + A[j] == target:
-                print(A[i], A[j])
-                return True
-    return False
-print (two_sum_brute_force(A, target))
+def brute_force(A, target):
+	for i in range(len(A)):
+		for j in range(i + 1, len(A)):
+			if A[i] + A[j] == target:
+				print (f'The elements {A[i]} and {A[j]} add up to {target}')
 
-"""
-
-for i in range(len(A)-1):
-	for j in range(i+1, len(A)):
-		print (j + 10)
+brute_force(A, target)
