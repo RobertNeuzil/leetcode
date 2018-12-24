d13ab742e2255f514555283c926cc9f50d0b2f1a
robert neuzil <robertneuzil@gmail.com>
2018-12-24 03:10:37 -0500

    regular expressions. These things are cool and very powerful

--- anagram.py
+++ anagram.py
@@ -6,24 +6,17 @@ str_4 = "The cat can't stop eating bacon"
 
 
 def anagram(str_1, str_2):
-	str_1 = str_1.replace(" ", "")
+	str_1 = str_1.replace( " ", "" )
 	str_2 = str_2.replace(" ", "")
 
 	if len(str_1) != len(str_2):
 		return False
-	
-	str_1 = str_1.upper()
-	str_2 = str_2.upper()
 
-	alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-	dict_1 = dict.fromkeys(list(alphabet), 0)
-	dict_2 = dict.fromkeys(list(alphabet), 0)
+	for c in str_1:
+		if c in str_2:
+			return True
+	return False
 
-	for i in range(len(str_1)):
-		dict_1[str_1[i]] += 1
-		dict_2[str_2[i]] += 1
-
-	return dict_1 == dict_2
 
 print(anagram(str_1, str_2))
 print(anagram(str_3, str_4))
