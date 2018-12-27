75283a57c4e5fbfb90f5ae40c3685b281e8e5acb
robert neuzil <robertneuzil@gmail.com>
2018-12-27 03:24:44 -0500

    practice makes perfect. there are no significant changes to this commit

--- recursionindexinglargefile.py
+++ recursionindexinglargefile.py
@@ -2,80 +2,62 @@
 perform a binary search of a list
 """
 
-#binary_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-#target = 1333
+binary_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+#target = 13
 
-def binary_search(binary_list, target, high, low):
-	while low <= high:
-		middle = (high + low) // 2
 
-		if binary_list[middle] == target:
-			print (f"the {target} is in the list")
-			return True
-		if target < binary_list[middle]:
-			return binary_search(binary_list, target, middle - 1, low)
-		if target > binary_list[middle]:
-			return binary_search(binary_list, target, len(binary_list) - 1, middle + 1)
-	print(f'The {target} is not in the list')
-	return False
 
-#palindrome_string = "radar"
+def binary(binary_list, target):
+    for x in binary_list:
+        if x == target:
+            return True
+    return False
 
-def is_palindrome(palindrome_string):
-	reversed_str = palindrome_string[::-1]
-	if reversed_str == palindrome_string:
-		return True
-	return False
+def binary_recursive(binary_list, target, high, low):
+    while low <= high:
+        middle = (low + high) // 2
 
-#upper_case_string = "theCarisred"
+        if binary_list[middle] == target:
+            print (f"{target} is part of the list")
+            return True
+        if binary_list[middle] < target:
+            return binary_recursive(binary_list, target, high, low + 1)
+        if binary_list[middle] > target:
+            return binary_recursive(binary_list, target, mid - 1, low)
+    print (f"{target} is not an element in the list")
+    return False
 
+#binary_recursive(binary_list, 2222222222222222, len(binary_list) - 1, 0)
+
+
+string = "robertNeuzil" 
 def find_upper(string):
-	index = 0
-	while index <= len(string) - 1:
-		if string[index].isupper():
-			return string[index]
-		else:
-			index += 1
-
-	return False
-
-def length_of_string(upper_case_string):
-	if upper_case_string == "":
-		#only returning massive number to illustrate concept
-		return 100000000
-	else:
-		return length_of_string(upper_case_string[1:]) + 1
+    index = 0
+    while index <= len(string):
+        for x in string:
+            if x[index].isupper():
+                return x
+        else:
+            index += 1
+    return False
 
 
 
+two_sum_list = [3, 4, 9, 11, 13]
+two_sum_target = 12
 
-sorted_list = [-2, -1, 1, 3, 5, 17, 19, 21, 23, 25]
-target = 8
+def find_two_sum(two_sum_target, two_sum_list, high, low):
+    while low <= high:
+        if two_sum_list[high] + two_sum_list[low] == two_sum_target:
+            print (f"{two_sum_list[high]} and {two_sum_list[low]} add up to {two_sum_target} ")
+            return True
+        if two_sum_target < two_sum_list[high] + two_sum_list[low]:
+            return find_two_sum(two_sum_target, two_sum_list, high - 1, low)
+        if two_sum_target > two_sum_list[high] + two_sum_list[low]:
+            return find_two_sum(two_sum_target, two_sum_list, high, low + 1)
+    print ("None of the items in the list can be added together to reach the target value")
+    return False
 
-
-def find_sum(sorted_list, target):
-	high = len(sorted_list) - 1
-	low = 0
-	
-	while low <= high:
-		if target == sorted_list[high] + sorted_list[low]:
-			return True
-		if target < sorted_list[high] + sorted_list[low]:
-			high -= 1
-		if target > sorted_list[high] + sorted_list[low]:
-			low += 1
-	return False
-
-def find_sum_recursion(sorted_list, target, high, low):
-	while low <= high:
-		if target == sorted_list[high] + sorted_list[low]:
-			return [sorted_list[high], sorted_list[low], "are equal to", target]
-		if target < sorted_list[high] + sorted_list[low]:
-			return find_sum_recursion(sorted_list, target, high - 1, low)
-		if target > sorted_list[high] + sorted_list[low]:
-			return find_sum_recursion(sorted_list, target, high, low + 1)
-	return False
-
-print(find_sum_recursion(sorted_list, target, len(sorted_list) - 1, 0))
+find_two_sum(12, two_sum_list, len(two_sum_list) - 1, 0)
 
 
