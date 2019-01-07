840da11b0c1b06ba65bbe084733e097ea90470be
robert neuzil <robertneuzil@gmail.com>
2019-01-07 07:33:12 -0500

    password generator appending to text file

--- unsecurepasswordgenerator.py
+++ unsecurepasswordgenerator.py
@@ -3,12 +3,18 @@ import random
 
 def generate_password(characterlength):
 	potential_character = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()?><:"|'
-	random_words = [' television', ' cat', ' car', ' message', ' L O S T' ]
+	random_words = [' television', ' cat', ' car', ' message', ' L O S T', 'newspaper', 'fridge', 
+	'walkietalkie', 'umbrella', 'coat hanger', 'washing machine', 
+	'currency', 'alabama', 'free firewood', 'eminem', 'soy farm', 'laptop', 'messaging service', 'car wash' ]
 	empty_str = ''
 	for i in range(characterlength):
 		empty_str = empty_str + potential_character[random.randrange(len(potential_character))] 
 	new_pass = empty_str + random_words[random.randrange(len(random_words))]
-	return new_pass
+	return new_pass + "\n"
 
-for n in range(1, 101):
-	print (generate_password(28))
+
+file = open("randompass.txt", "a")
+
+for n in range(1, 1000001):
+	file.write(generate_password(28),)
+file.close()
