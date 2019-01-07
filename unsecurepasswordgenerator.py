40e64d1180ab6b2095c7d04d5528eb20ae5640f7
robert neuzil <robertneuzil@gmail.com>
2019-01-07 05:46:28 -0500

    update password generator

--- unsecurepasswordgenerator.py
+++ unsecurepasswordgenerator.py
@@ -3,11 +3,12 @@ import random
 
 def generate_password(characterlength):
 	potential_character = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()?><:"|'
-	random_word = 'television'
+	random_words = [' television', ' cat', ' car', ' message', ' L O S T' ]
 	empty_str = ''
 	for i in range(characterlength):
 		empty_str = empty_str + potential_character[random.randrange(len(potential_character))] 
-	new_pass = empty_str + random_word
+	new_pass = empty_str + random_words[random.randrange(len(random_words))]
 	return new_pass
 
-print (generate_password(28))
+for n in range(1, 101):
+	print (generate_password(28))
