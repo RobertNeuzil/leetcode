fa2da642fa42fdc3f8c1e40fc9d5173ae4e1e5c1
robert neuzil <robertneuzil@gmail.com>
2019-01-13 18:14:51 -0500

    recursively

--- twosums.py
+++ twosums.py
@@ -1,18 +1,16 @@
 sorted_list = [-2, -1, 1, 3, 5, 17, 19, 21, 23, 25]
-target = 8
+target = 48
 
 
-def find_the_sum(target, sorted_list):
-	low = 0
-	high = len(sorted_list) - 1
+def find_the_sum(target, sorted_list, low, high):
+	
 	while low < high:
 		if sorted_list[low] + sorted_list[high] == target:
-			print (sorted_list[low], "and", sorted_list[high], "equal", target)
 			return True
 		if target < sorted_list[low] + sorted_list[high]:
-			high = high - 1
+			return find_the_sum(target, sorted_list, 0, high - 1)
 		if target > sorted_list[low] + sorted_list[high]:
-			low = low + 1
+			return find_the_sum(target, sorted_list, low + 1, high)
 	return False
 
-print(find_the_sum(target, sorted_list))
+print(find_the_sum(18, sorted_list, 0, len(sorted_list) - 1))
