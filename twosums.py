075fac76565c78d6b4eb9a6b06d3fe356e1b08f5
robert neuzil <robertneuzil@gmail.com>
2019-01-13 15:47:58 -0500

    two sum, problem

--- twosums.py
+++ twosums.py
@@ -2,18 +2,17 @@ sorted_list = [-2, -1, 1, 3, 5, 17, 19, 21, 23, 25]
 target = 8
 
 
-def find_sum(sorted_list, target):
-	high = len(sorted_list) - 1
+def find_the_sum(target, sorted_list):
 	low = 0
-	
-	while low <= high:
-		if target == sorted_list[high] + sorted_list[low]:
+	high = len(sorted_list) - 1
+	while low < high:
+		if sorted_list[low] + sorted_list[high] == target:
+			print (sorted_list[low], "and", sorted_list[high], "equal", target)
 			return True
-		if target < sorted_list[high] + sorted_list[low]:
-			high -= 1
-		if target > sorted_list[high] + sorted_list[low]:
-			low += 1
+		if target < sorted_list[low] + sorted_list[high]:
+			high = high - 1
+		if target > sorted_list[low] + sorted_list[high]:
+			low = low + 1
 	return False
 
-
-print(find_sum(sorted_list, 8))
+print(find_the_sum(target, sorted_list))
